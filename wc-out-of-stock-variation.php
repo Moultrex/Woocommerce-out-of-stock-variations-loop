d99af8d0efe537649b65e9494442c07ef5ca2015
Moultrex <33543155+Moultrex@users.noreply.github.com>
2018-10-14 15:01:44 +0300

    Suppress notice  PHP notice by adding isset
    line 10

--- wc-out-of-stock-variation.php
+++ wc-out-of-stock-variation.php
@@ -1,24 +1,42 @@
-<?php
 add_action( 'woocommerce_before_shop_loop_item_title', 'wk_out_of_stock_variations_loop' );
-
 function wk_out_of_stock_variations_loop(){
     global $product;
     if ( $product->product_type == 'variable' ) { // if variation product is out of stock
-
         $available = $product->get_available_variations();
         if ( $available )foreach ( $available as $instockvar ) {
-            if ( isset($instockvar['attributes']['attribute_pa_kleur'] ) ) {
+            if ( isset($instockvar['attributes']['attribute_pa_megethos'] ) ) {
               
-		if ( ( $instockvar['attributes']['attribute_pa_kleur'] == $_GET['filter_kleur'] ) && (!$instockvar['max_qty']>0) ) {
+			  
+		if(isset($_GET['filter_megethos'])){
+		   
+		   $destostock = $_GET['filter_megethos'];
+			  
+		  
+		  $array = explode(',', $destostock);
+		  
+
+
+    if (in_array($instockvar['attributes']['attribute_pa_megethos'], $array )&& (!$instockvar['max_qty']>0)){
 		   global $product;
 		   $id = $product->get_id();
-	           echo "<style>.post-$id{display: none}</style>";
-		   // echo  '<p style="color:red;font-weight: bold;">OUT OF STOCK</p>';
-                }
-            }
+	  
+			echo "<style>.post-$id{display: none}</style>";
+
+	}
+		  else{
+			
+		  if (in_array($instockvar['attributes']['attribute_pa_megethos'], $array )&& ($instockvar['max_qty']>0)){
+		   global $product;
+		   $id = $product->get_id();
+	  
+			echo "<style>.post-$id{display: block !important}</style>";
+		  
+		  }
+		  }
+            
         }
     }	
-    if ( !$product->is_in_stock() ) { // if single product is out of stock
-        echo  '<p style="color:red;font-weight: bold;">OUT OF STOCK</p>';
-    }
+ 
+}
+	}
 }
