93bc918d250aca5abbd21e738d46459d972c448c
Moultrex <33543155+Moultrex@users.noreply.github.com>
2019-07-15 16:47:18 +0300

    Update wc-out-of-stock-variation.php
    I re wrote the script.
    
    Now the filtering is done before the page load at query level.
    
    Change filter_megethos with the attribute filter of your url and attribute_pa_megethos with the attribute_pa_ of your attribute that you want to filter.

--- wc-out-of-stock-variation.php
+++ wc-out-of-stock-variation.php
@@ -1,42 +1,69 @@
-add_action( 'woocommerce_before_shop_loop_item_title', 'wk_out_of_stock_variations_loop' );
-function wk_out_of_stock_variations_loop(){
-    global $product;
-    if ( $product->product_type == 'variable' ) { // if variation product is out of stock
-        $available = $product->get_available_variations();
-        if ( $available )foreach ( $available as $instockvar ) {
-            if ( isset($instockvar['attributes']['attribute_pa_megethos'] ) ) {
-              
-			  
-		if(isset($_GET['filter_megethos'])){
+add_action('pre_get_posts','remove_products');
+
+function remove_products($query) {
+   if(! is_admin() && $query->is_main_query()){
+	 
+	 
+	if(isset($_GET['filter_megethos'])){
 		   
-		   $destostock = $_GET['filter_megethos'];
-			  
-		  
-		  $array = explode(',', $destostock);
-		  
-
-
-    if (in_array($instockvar['attributes']['attribute_pa_megethos'], $array )&& (!$instockvar['max_qty']>0)){
-		   global $product;
-		   $id = $product->get_id();
+	$fltrmeg = sanitize_key($_GET['filter_megethos']);
 	  
-			echo "<style>.post-$id{display: none}</style>";
+	$xplfltrmeg = explode(',', $fltrmeg);
+  
+ 
+   	$current_category = get_queried_object();
+
+      $args = array(
+         'post_type'                => 'product',
+         'post_status'              => 'publish',
+         'posts_per_page'           => get_option('posts_per_page'),
+         'tax_query'                => array(
+            array(
+               'taxonomy'           => 'product_cat',
+               'field'              => 'term_id',
+               'terms'              => $current_category->term_id,
+            )
+         )
+      );
+
+      $loop = get_posts($args);
+	  $post__in1 = array();
+	  $post__in2 = array();
+	  $post__in3 = array();
+
+      foreach ($loop as $post) {
+
+         //Product object
+         $product = wc_get_product($post->ID);
+
+         //Get product info i.e variations > meta keys
+         $product_variations = $product->get_children();		
+
+         foreach ($product_variations as $variation) {
+
+            // Store only the meta keys needed
+            $product_attribute = get_post_meta($variation, 'attribute_pa_megethos'); // 'attribute slug name'
+            $stock_status = get_post_meta($variation, '_stock_status'); // 'in stock / out of stock'
+			 
+			 if(in_array($product_attribute[0], $xplfltrmeg)){
+			   if(in_array('outofstock', $stock_status)) {
+			   	$post__in2[] = $post->ID;
+			 }
+			   if(in_array('instock', $stock_status)) {
+			   	$post__in3[] = $post->ID;
+			 }
+			 
+		   }
+		 }
+	  }
+	 
+	 $post__in1 = array_diff($post__in2, $post__in3);
+
+      //Exclude the product
+      if(is_array($post__in1) && $post__in1 != null) {
+         $query->set('post__not_in', $post__in1);
+      }
 
 	}
-		  else{
-			
-		  if (in_array($instockvar['attributes']['attribute_pa_megethos'], $array )&& ($instockvar['max_qty']>0)){
-		   global $product;
-		   $id = $product->get_id();
-	  
-			echo "<style>.post-$id{display: block !important}</style>";
-		  
-		  }
-		  }
-            
-        }
-    }	
- 
-}
 	}
 }
